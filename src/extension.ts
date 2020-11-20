90da11b28c0ff7ec21ed1d7ac0bd9bfb89fe07c2
Tom <tom@tomdotbat.dev>
2020-11-20 00:24:16 +0000

    Replace != with the Lua operator ~=

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,26 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+import * as vscode from "vscode";
+
+/* eslint-disable curly */
 export function activate(context: vscode.ExtensionContext) {
+	console.log("Better Lua Syntax: Activated.");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "better-lua-syntax" is now active!');
+	vscode.workspace.onDidSaveTextDocument(function(document: vscode.TextDocument) {
+		if ((document.languageId !== "lua" && document.languageId !== "glua")
+			|| document.uri.scheme !== "file") return;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('better-lua-syntax.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+		const documentText = document.getText();
+		const editor = vscode.window.activeTextEditor;
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Better Lua Syntax!');
+		editor?.edit(editBuilder => { //Replace != with ~=
+			const rxNotEquals = new RegExp("!=", "gi");
+
+			while (rxNotEquals.exec(documentText)) {
+				editBuilder.replace(new vscode.Range(
+					document.positionAt(rxNotEquals.lastIndex - 2),
+					document.positionAt(rxNotEquals.lastIndex)
+				), "~=");
+			};
+		});
 	});
-
-	context.subscriptions.push(disposable);
-}
-
-// this method is called when your extension is deactivated
-export function deactivate() {}
+};
